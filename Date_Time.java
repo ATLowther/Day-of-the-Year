44000f7e0f1227fa6d306603a1411bb011f0f3c1
ATLowther <ATLowther@users.noreply.github.com>
2017-02-07 12:24:17 -0500

    Add files via upload

--- Date_Time.java
+++ Date_Time.java
@@ -4,12 +4,58 @@ public class Date_Time {
 
 	public static void main(String[] args) {
 		
+		final HashMap<String, Double> timeZones = new HashMap<String, Double>(); {
+			
+			timeZones.put("LINT", 14.0);
+			timeZones.put("CHADT", 13.75);
+			timeZones.put("NZDT", 13.0);
+			timeZones.put("ANAT", 12.0);
+			timeZones.put("AEDT", 11.0);
+			timeZones.put("ACDT", 10.5);
+			timeZones.put("AEST", 10.0);
+			timeZones.put("ACST", 9.5);
+			timeZones.put("JST", 9.0);
+			timeZones.put("ACWST", 8.75);
+			timeZones.put("PYT", 8.5);
+			timeZones.put("CST", 8.0);
+			timeZones.put("WIB", 7.0);
+			timeZones.put("MMT", 6.5);
+			timeZones.put("BST", 6.0);
+			timeZones.put("NPT", 5.75);
+			timeZones.put("IST", 5.5);
+			timeZones.put("UZT", 5.0);
+			timeZones.put("AFT", 4.5);
+			timeZones.put("GST", 4.0);
+			timeZones.put("IRST", 3.5);
+			timeZones.put("MSK", 3.0);
+			timeZones.put("EET", 2.0);
+			timeZones.put("CET", 1.0);
+			timeZones.put("GMT", 0.0);
+			timeZones.put("CVT", -1.0);
+			timeZones.put("BRST", -2.0);
+			timeZones.put("ART", -3.0);
+			timeZones.put("NST", -3.5);
+			timeZones.put("VET", -4.0);
+			timeZones.put("EST", -5.0);
+			timeZones.put("CST", -6.0);
+			timeZones.put("MST", -7.0);
+			timeZones.put("PST", -8.0);
+			timeZones.put("AKST", -9.0);
+			timeZones.put("MART", -9.5);
+			timeZones.put("HAST", -10.0);
+			timeZones.put("NUT", -11.0);
+			timeZones.put("AOE", -12.0);
+
+			
+			}
+
+		
 		Scanner input = new Scanner(System.in);
 		System.out.print("What time zone are you in? ");
 		String userTimeZone = input.nextLine().toUpperCase();
 		input.close();
 		
-		int userTime = timeZones.get(userTimeZone);	
+		double userTime = timeZones.get(userTimeZone);	
 		
 		double totalMilliseconds = System.currentTimeMillis();
 		
@@ -24,7 +70,7 @@ public class Date_Time {
 		double currentHour = (totalHours + userTime) % 24;			
 				
 		
-		double totalDays = totalHours / 24;
+		double totalDays = (totalHours + userTime) / 24;
 		double currentDay = totalDays % 365;
 		currentDay = Math.ceil(currentDay);
 
